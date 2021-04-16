20cfe761b7ae37b4220ee15d74768bc210cd0628
Shameel Fazul <shameel.fazul@mail.com>
2021-04-16 17:28:03 +0530

    src

--- bot.js
+++ bot.js
@@ -1,20 +1,21 @@
 const http = require('http');
+const handler = require('serve-handler');
+const puppeteer = require("puppeteer")
 const socketio = require('socket.io');
 const fs  = require("fs")
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const download = require('image-downloader');
+ 
+const server = http.createServer((request, response) => {
+  return handler(request, response);
+})
 
-
-const requestListener = (req, res) => {
-    res.writeHead(200);
-    fs.createReadStream('index.html').pipe(res)
-}
-
-const server = http.createServer(requestListener);
 const io = socketio(server);
-server.listen(process.env.PORT || 3000)
-console.log('The server is listening..')
+ 
+server.listen(3000, () => {
+  console.log('Running at http://localhost:3000');
+});
 
 
 io.on('connection', socket => {
@@ -24,19 +25,35 @@ io.on('connection', socket => {
 
     client.on('message', async (msg) => {
         if (msg.content.length < 1) {
+            const browser = await puppeteer.launch()
+  const page = await browser.newPage()
             const image = await msg.attachments.toJSON();
-            const options = { url: image[0].url, dest: './images' }
+            const options = { url: image[0].url, dest: './testimage' }
             download.image(options)
                 .then(({ filename }) => {
+                    console.log(filename)
                     const file = fs.readFileSync(`./${filename}`)
-                    socket.emit('predict', file.toString('base64'))
+                    ;(async () => {
+                        
+                        await page.goto("http://localhost:3000/public/")
+                        console.log(await page.evaluate("ml5.version")) // prints "0.5.0"
+                        page.on("console", msg => console.log(">", msg.text()))
+                        socket.emit('predict', file.toString('base64'))
+                      
+                        // This allows to save the model when classifier.save() is called.
+                        // downloadPath is the folder in which the model will be saved.
+                        // await page._client.send("Page.setDownloadBehavior", {
+                        //   behavior: "allow",
+                        //   downloadPath: "./",
+                        // })
+                    })
                 })
                 .catch((err) => console.error(err))
             socket.on('result', data => {
                 return msg.reply(`I'm ${Math.floor(data[0].confidence * 100)}% confident that it's a ${data[0].label}`)
-        });
+            });
         }
     });
 });
 
-client.login('#');
+client.login('ODMxMDkwMjc2NTM3MzM1ODM4.YHQLIg.yfoLT8hBrQ9PdIGreCk6AlpoP8o');
---
+++ ml.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const handler = require('serve-handler');
+const puppeteer = require("puppeteer");
+const fs  = require("fs");
+var url = require('url');
+const socketio = require('socket.io');
+const download = require('image-downloader');
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const server = http.createServer((request, response) => {
+    return handler(request, response);
+});
+server.listen(process.env.PORT || 3000, () => {
+    console.log('[ML] > server is running');
+});
+const io = socketio(server);
+
+
+client.on('message', async (msg) => {
+    if (msg.content.length < 1) {
+        console.log('[ML] > prediction request detected')
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
+        await page.goto("http://localhost:3000/public/")
+        console.log('[ML] > page has loaded')
+        page.on("console", async (res) => {
+          msg.reply(res.text())
+          console.log("[ML] > page closed")
+          await browser.close();
+          return;
+        })
+
+        io.on('connection', async (socket) => {
+            const attachment = await msg.attachments.toJSON();
+            const options = { url: attachment[0].url, dest: './images' }
+            download.image(options)
+                .then(({ filename }) => {
+                    ;(async () => {  
+                        const version = await page.evaluate("ml5.version");
+                        console.log(`[ML] > ml5 ${version} has loaded.`);
+                        const image_file = fs.readFileSync(`./${filename}`);
+                        socket.emit('predict', image_file.toString('base64'));
+                        fs.unlinkSync(`./${filename}`);
+                    })()
+                })
+                .catch((err) => console.error(err))
+        });
+    } else {
+        return;
+    }
+});
+
+client.login('ODMxMDkwMjc2NTM3MzM1ODM4.YHQLIg.yfoLT8hBrQ9PdIGreCk6AlpoP8o')
